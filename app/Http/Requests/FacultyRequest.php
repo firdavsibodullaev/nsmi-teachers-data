b9bac48c67207ec48691204c157fdc173a2927af
Firdavs <firdavs3253@gmail.com>
2021-09-11 23:38:45 +0500

    finished faculty request

--- app/Http/Requests/FacultyRequest.php
+++ app/Http/Requests/FacultyRequest.php
@@ -11,9 +11,9 @@ class FacultyRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,10 +21,31 @@ class FacultyRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
+        return $this->getRules();
+    }
+
+    private function getRules(): array
+    {
+        if ($this->isMethod('post')) {
+            return [
+                'FullNameUz' => 'required|unique:faculties,FullNameUz|max:255',
+                'FullNameOz' => 'required|unique:faculties,FullNameOz|max:255',
+                'FullNameRu' => 'required|unique:faculties,FullNameRu|max:255',
+                'ShortNameUz' => 'required|unique:faculties,ShortNameUz|max:50',
+                'ShortNameOz' => 'required|unique:faculties,ShortNameOz|max:50',
+                'ShortNameRu' => 'required|unique:faculties,ShortNameRu|max:50',
+            ];
+        }
+        $facultyId = $this->query('faculty');
         return [
-            //
+            'FullNameUz' => "required|unique:faculties,FullNameUz,{$facultyId},Id|max:255",
+            'FullNameOz' => "required|unique:faculties,FullNameOz,{$facultyId},Id|max:255",
+            'FullNameRu' => "required|unique:faculties,FullNameRu,{$facultyId},Id|max:255",
+            'ShortNameUz' => "required|unique:faculties,ShortNameUz,{$facultyId},Id|max:50",
+            'ShortNameOz' => "required|unique:faculties,ShortNameOz,{$facultyId},Id|max:50",
+            'ShortNameRu' => "required|unique:faculties,ShortNameRu,{$facultyId},Id|max:50",
         ];
     }
 }
