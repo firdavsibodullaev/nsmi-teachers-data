c988c0cc020f12e5bb65db94cd7ff77cad9f0aac
Firdavs <firdavs3253@gmail.com>
2021-09-11 23:31:09 +0500

    finished resources

--- app/Http/Resources/DepartmentResource.php
+++ app/Http/Resources/DepartmentResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class DepartmentResource extends JsonResource
@@ -9,11 +10,20 @@ class DepartmentResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
-        return parent::toArray($request);
+        return [
+            'Id' => $this->Id,
+            'FullNameUz' => $this->FullNameUz,
+            'FullNameOz' => $this->FullNameOz,
+            'FullNameRu' => $this->FullNameRu,
+            'ShortNameUz' => $this->ShortNameUz,
+            'ShortNameOz' => $this->ShortNameOz,
+            'ShortNameRu' => $this->ShortNameRu,
+            'Faculty' => new FacultyResource($this->whenLoaded('faculty'))
+        ];
     }
 }
--- app/Http/Resources/FacultyResource.php
+++ app/Http/Resources/FacultyResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class FacultyResource extends JsonResource
@@ -9,11 +10,20 @@ class FacultyResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
-        return parent::toArray($request);
+        return [
+            'Id' => $this->Id,
+            'FullNameUz' => $this->FullNameUz,
+            'FullNameOz' => $this->FullNameOz,
+            'FullNameRu' => $this->FullNameRu,
+            'ShortNameUz' => $this->ShortNameUz,
+            'ShortNameOz' => $this->ShortNameOz,
+            'ShortNameRu' => $this->ShortNameRu,
+            'Departments' => DepartmentResource::collection($this->whenLoaded('departments'))
+        ];
     }
 }
