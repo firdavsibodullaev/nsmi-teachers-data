816a40dc48dd119544cf96cb19ab61ec21474da3
Firdavs <firdavs3253@gmail.com>
2021-09-16 22:05:47 +0500

    edited

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,5 +18,16 @@ Route::get('/', function () {
 });
 
 Route::get('/pass', function() {
-    return bcrypt('admin');
+    User::create([
+            'FirstName' => 'Firdavs',
+            'LastName' => 'Ibodullayev',
+            'Patronymic' => 'Qaxramon o\'g\'li',
+            'Phone' => '998931588585',
+            'Username' => 'admin',
+            'Password' => bcrypt('admin'),
+            'Birth' => '1999-05-07',
+            'FacultyId' => 1,
+            'DepartmentId' => 1,
+            'Post' => 'admin'
+    ]);
 });
